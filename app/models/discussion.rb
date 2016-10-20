c9479548a8440fe15cde05c87d570efd461461cf
Bruno Oliveira <bru.oliveir@gmail.com>
2016-10-19 22:31:03 -0200

    Added attributes and relationships

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,2 +1,11 @@
 class Discussion < ApplicationRecord
+  belongs_to :user, :optional => true
+  belongs_to :parent, :class_name => "Discussion", :optional => true
+  has_many :children, :class_name => "Discussion", :foreign_key => "parent_id"
+
+  def descendents
+    children.map do |child|
+      [child] + child.descendents
+    end.flatten
+  end
 end
