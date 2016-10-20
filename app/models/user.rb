2164771d7d5d155a6aee12c1c32a32f756c9c0ec
Bruno Oliveira <bru.oliveir@gmail.com>
2016-10-20 01:20:37 -0200

    Added validations and tests for User attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_many :discussions
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
 end
