1290c69635a7fe3607ea198b6152bd5c42babfdb
Bruno Oliveira <bru.oliveir@gmail.com>
2016-10-20 00:58:20 -0200

    Added discussion model tests

--- test/models/discussion_test.rb
+++ test/models/discussion_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class DiscussionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @root = Discussion.create(:title => "Root thread")
+  end
+
+  test "should not be deleted" do
+    assert_raises(ActiveRecord::ReadOnlyRecord) do
+      @root.destroy
+    end
+  end
+
+  test "should not be updated" do
+    assert_raises(ActiveRecord::ReadOnlyRecord) do
+      @root.update_attributes(:title => "Updated title")
+    end
+
+    assert_raises(ActiveRecord::ReadOnlyRecord) do
+      @root.title = "Updated title"
+      @root.save
+    end
+  end
+
+  test "should have descendents 1 level deep" do
+    subthreadA = Discussion.create(:title => "Root.A", :parent => @root)
+    subthreadB = Discussion.create(:title => "Root.B", :parent => @root)
+    
+    assert @root.descendents == [subthreadA, subthreadB]
+  end
+
+  test "should have descendents 2 levels deep" do
+    subthreadA = Discussion.create(:title => "Root.A", :parent => @root)
+    subthreadB = Discussion.create(:title => "Root.A.B", :parent => subthreadA)
+    
+    assert @root.descendents == [subthreadA, subthreadB]
+  end
+
+  test "should have descendents 3 levels deep" do
+    subthreadA = Discussion.create(:title => "Root.A", :parent => @root)
+    subthreadB = Discussion.create(:title => "Root.A.B", :parent => subthreadA)
+    subthreadC = Discussion.create(:title => "Root.A.B.C", :parent => subthreadB)
+    
+    assert @root.descendents == [subthreadA, subthreadB, subthreadC]
+  end
 end
