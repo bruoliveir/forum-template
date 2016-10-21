f8462d913dd60cbe3906718698019ec9561059ac
Bruno Oliveira <bru.oliveir@gmail.com>
2016-10-21 01:00:29 -0200

    Added discussion seeds (without user_id)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def create_discussion(discussion, parent)
+  d = Discussion.create(:title => discussion['title'], :body => discussion['body'], :parent => parent)
+  discussion['children'].each do |discussion|
+    create_discussion(discussion, d)
+  end unless discussion['children'].nil?
+end
+
+[
+  # L1
+  { 'title' => 'Thread 1', 'body' => 'Hello, this is root thread n. 1.', 'children' => [
+    # L2
+    { 'title' => 'Comment 1', 'body' => 'Hello, this is comment 1 on root thread n. 1.', 'children' => [
+      # L3
+      { 'title' => 'Comment 2', 'body' => 'Hello, this is comment 2 as a reply to comment 1 on root thread n. 1.' },
+      { 'title' => 'Comment 3', 'body' => 'Hello, this is comment 3 as a reply to comment 1 on root thread n. 1.', 'children' => [
+        # L4
+        { 'title' => 'Comment 4', 'body' => 'Hello, this is comment 4 as a reply to comment 3 on comment n. 1.' }
+      ]},
+      # L3
+      { 'title' => 'Comment 5', 'body' => 'Hello, this is comment 5 as a reply to comment 1 on root thread n. 1.' }
+    ]},
+    # L2
+    { 'title' => 'Comment 6', 'body' => 'Hello, this is comment 6 on root thread n. 1.' }
+  ]},
+  # L1
+  { 'title' => 'Thread 2', 'body' => 'Hello, this is root thread n. 2.', 'children' => [
+    # L2
+    { 'title' => 'Comment 1', 'body' => 'Hello, this is comment 1 on root thread n. 2.' },
+    { 'title' => 'Comment 2', 'body' => 'Hello, this is comment 2 on root thread n. 2.' }
+  ]}
+].each do |discussion|
+  create_discussion(discussion, nil)
+end
