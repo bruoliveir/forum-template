c8e422adda2b74f43163cda55c03ab4c1e9e2e5e
Bruno Oliveira <bru.oliveir@gmail.com>
2016-10-22 03:23:10 -0200

    Updated routes to render json by default

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :discussions
-  resources :users
+  resources :discussions, defaults: {format: :json}, only: ['index', 'create', 'new', 'show']
+  resources :users, defaults: {format: :json}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
